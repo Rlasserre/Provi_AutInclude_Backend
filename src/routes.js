b0c6bdbd46611e511d91f5d01c5817a90e35535c
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2021-11-28 21:30:43 -0300

    add: logar

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 //const filtroLogin = require('./filtros/filtroLogin');
 
 const cadastroUsuario = require('./Controllers/usuario/cadastro');
-//const funcionalidadeUsuario = require('./controladores');
+const logar = require('./Controllers/usuario/login');
 
 const router = express();
 
-//router.post('/login', funcionalidadeCliente.fazerLogin);
+router.post('/login', logar);
 router.post('/usuario', cadastroUsuario);
 
 
